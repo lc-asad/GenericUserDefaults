474ad9c9d66509c07e3be647b4b760741ac27321
Asad Ullah <asad_622@hotmail.com>
2023-01-14 16:15:23 +0500

    Initial project setup

---
+++ GenericUserDefaultsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericUserDefaultsExample
+//
+//  Created by Asad Ullah on 14/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
